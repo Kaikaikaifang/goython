230732f9cdd7e6cc6730aa0d6997abe80c96bf30
Kaikai <98205900+Kaikaikaifang@users.noreply.github.com>
2024-12-04 15:53:31 +0800

    refactor(sdk): mock 2 real

--- service_grpc/__init__.py
+++ service_grpc/__init__.py
@@ -1,5 +1,13 @@
+from typing import Optional
+
+from .sdk import Run, init, finish
+
 from . import calculator_client
 from . import proto
 
-name = "service_grpc"
-__all__ = ["calculator_client", "proto"]
+name: str = "service_grpc"
+
+run: Optional[Run] = None
+
+
+__all__ = ["calculator_client", "proto", "init", "finish"]
--- service_grpc/calculator_client/client.py
+++ service_grpc/calculator_client/client.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 import logging
 
 import grpc
+
+import service_grpc
 from service_grpc.proto import calculator_pb2, calculator_pb2_grpc
 
 
@@ -12,8 +14,13 @@ def add():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
+    run = service_grpc.run
+    if run is None:
+        print("Please init the service first.")
+        return
+
     print("Will try to calculate ...")
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel(run.target) as channel:
         stub = calculator_pb2_grpc.CalculatorStub(channel)
         response: calculator_pb2.AddResponse = stub.Add(calculator_pb2.AddRequest(a=1, b=2))
     print("Calculator client: 1 + 2 = " + str(response.result))
@@ -21,4 +28,5 @@ def add():
 
 if __name__ == "__main__":
     logging.basicConfig()
+    service_grpc.init()
     add()
